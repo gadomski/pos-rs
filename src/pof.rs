1c50a12cdbb2fadabdf4302ed5b2ae6b10c7c922
Pete Gadomski <pete.gadomski@gmail.com>
2018-03-05 08:14:19 -0700

    Add some space to the pof struct

--- src/pof.rs
+++ src/pof.rs
@@ -21,49 +21,70 @@ use units::Radians;
 pub struct Reader<R: Read + Seek> {
     /// The average time interval between points.
     pub avgint: f64,
+
     /// The name of the company that produced this file.
     pub company: [u8; 32],
+
     /// The day this file was written.
     pub day: u16,
+
     /// The name of the device that collected this file.
     pub device: [u8; 32],
+
     /// The standard deviation of the time interval between points.
     pub devint: f64,
+
     /// The number of points in this file.
     pub entries: i64,
+
     /// The location that this file was collected.
     pub location: [u8; 16],
+
     /// The maximum altitude in this file.
     pub maxalt: f64,
+
     /// The maximum time interval between points in this file.
     pub maxint: f64,
+
     /// The maximum latitude.
     pub maxlat: f64,
+
     /// The maximum longitude.
     pub maxlon: f64,
+
     /// The minimum altitude.
     pub minalt: f64,
+
     /// The minimum latitude.
     pub minlat: f64,
+
     /// The minimum longitude.
     pub minlon: f64,
+
     /// The month this file was collected.
     pub month: u16,
+
     /// The name of this project.
     pub project: [u8; 32],
+
     /// Information about the time stamps in this file.
     pub timeinfo: TimeInfo,
+
     /// The units of the times in this file.
     pub timeunit: TimeUnit,
+
     /// This file's time zone.
     ///
     /// I have no idea if this is a real timezone->string code, or if it is just an arbitrary
     /// string.
     pub timezone: [u8; 16],
+
     /// The version of this file.
     pub version: Version,
+
     /// The year this file was collected.
     pub year: u16,
+
     reader: R,
     position: i64,
 }
